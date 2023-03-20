1fdabe546556212ad3f7efe38a2e9482f08854e9
30bit <m30bit@gmail.com>
2023-03-21 00:29:11 +0300

    Replace old matches block

--- hitori-macros/src/expand/impl_.rs
+++ hitori-macros/src/expand/impl_.rs
@@ -1,5 +1,4 @@
 mod matches_block;
-mod __new_matches_block;
 
 use crate::parse::{self, impl_::Config};
 use proc_macro2::{Ident, TokenStream};
@@ -24,7 +23,7 @@ fn matches_sig(
             <#capture_arg as #hitori_ident::CaptureMut>::Error
         >
         where
-            __I: IntoIterator<Item = (#idx_arg, #ch_arg)>,
+            __I: core::iter::IntoIterator<Item = (#idx_arg, #ch_arg)>,
             __I::IntoIter: Clone,
     }
 }
@@ -32,7 +31,7 @@ fn matches_sig(
 pub struct Input<'a> {
     pub hitori_ident: &'a Ident,
     pub config: &'a Config,
-    pub generic_params: &'a Punctuated<GenericParam, Token![,]>,
+    pub generic_params: &'a mut Punctuated<GenericParam, Token![,]>,
     pub where_clause: Option<&'a WhereClause>,
     pub self_path: &'a Path,
     pub trait_ident: &'a Ident,
@@ -45,7 +44,7 @@ impl<'a> Input<'a> {
         Self {
             hitori_ident,
             config: &parsed.impl_config,
-            generic_params: &parsed.generic_params,
+            generic_params: &mut parsed.generic_params,
             where_clause: parsed.where_clause.as_ref(),
             self_path: &parsed.self_path,
             trait_ident: &parsed.trait_ident,
@@ -53,29 +52,33 @@ impl<'a> Input<'a> {
             const_expr: &mut parsed.const_expr,
         }
     }
+}
 
-    fn expand_one(
-        Self {
-            hitori_ident,
-            config,
-            generic_params,
-            where_clause,
-            self_path,
-            trait_ident,
-            trait_args,
-            ..
-        }: &Self,
-        matches_block: &TokenStream,
-    ) -> TokenStream {
-        let matches_sig = matches_sig(hitori_ident, trait_args, matches!(config, Config::ExprMut));
-        quote! {
-            impl<#generic_params> #hitori_ident::#trait_ident<#(#trait_args),*> for #self_path
-            #where_clause
-            {
-                #matches_sig { #matches_block }
-            }
-        }
+fn expand_header(
+    Input {
+        hitori_ident,
+        generic_params,
+        where_clause,
+        self_path,
+        trait_ident,
+        trait_args,
+        ..
+    }: &Input,
+) -> TokenStream {
+    quote! {
+        impl<#generic_params> #hitori_ident::#trait_ident<#(#trait_args),*> for #self_path
+        #where_clause
     }
+}
+
+fn expand_expr_mut_from_expr(input: &Input) -> TokenStream {
+    let matches_sig = matches_sig(input.hitori_ident, input.trait_args, true);
+    let mut output = expand_header(&input);
+    let hitori_ident = input.hitori_ident;
+    output.extend(quote! {
+        #matches_sig { <Self as #hitori_ident::Expr>::matches(self, capture, start, iter) }
+    });
+    output
 }
 
 pub struct Output {
@@ -83,25 +86,52 @@ pub struct Output {
     pub capture_fn_idents: Vec<Ident>,
 }
 
+fn expand_with_matches_block(input: &mut Input) -> syn::Result<Output> {
+    let mut output_tokens = expand_header(&input);
+    matches_block::Input {
+        generic_params: input.generic_params,
+        where_clause: input.where_clause,
+        trait_args: input.trait_args,
+        self_path: input.self_path,
+        expr: input.const_expr,
+        is_mut: matches!(input.config, Config::ExprMut),
+    }
+    .try_into()
+    .map(|matches_block::Output { tokens, capture }| {
+        let matches_sig = matches_sig(
+            input.hitori_ident,
+            input.trait_args,
+            matches!(input.config, Config::ExprMut),
+        );
+        output_tokens.extend(quote! { #matches_sig { #tokens } });
+        Output {
+            tokens: output_tokens,
+            capture_fn_idents: capture,
+        }
+    })
+}
+
 impl<'a> TryFrom<Input<'a>> for Output {
     type Error = syn::Error;
 
     fn try_from(mut input: Input<'a>) -> Result<Self, Self::Error> {
-        let matches_block::Output {
-            tokens: matches_block,
-            capture: capture_fn_idents,
-        } = input.const_expr.try_into()?;
-        let mut tokens = Input::expand_one(&input, &matches_block);
         if input.config.and_expr_mut() {
             let trait_ident = format_ident!("ExprMut");
             let impl_config = Config::ExprMut;
-            input.trait_ident = unsafe { mem::transmute(&trait_ident) };
-            input.config = unsafe { mem::transmute(&impl_config) };
-            tokens.extend(Input::expand_one(&input, &matches_block));
+            let other_trait_ident = mem::replace(&mut input.trait_ident, unsafe {
+                mem::transmute(&trait_ident)
+            });
+            let other_config =
+                mem::replace(&mut input.config, unsafe { mem::transmute(&impl_config) });
+            let tokens = expand_expr_mut_from_expr(&input);
+            input.trait_ident = other_trait_ident;
+            input.config = other_config;
+            expand_with_matches_block(&mut input).map(|mut output| {
+                output.tokens.extend(tokens);
+                output
+            })
+        } else {
+            expand_with_matches_block(&mut input)
         }
-        Ok(Self {
-            tokens,
-            capture_fn_idents,
-        })
     }
 }
--- hitori-macros/src/expand/impl_/matches_block.rs
+++ hitori-macros/src/expand/impl_/matches_block.rs
@@ -1,9 +1,14 @@
 use proc_macro2::{Ident, TokenStream};
-use quote::quote;
-use std::ops::AddAssign;
-use syn::{parse::Parse, punctuated::Punctuated, Expr, ExprRange, Token};
+use quote::{format_ident, quote, ToTokens};
+use syn::{
+    parse::Parse, parse_quote, punctuated::Punctuated, Expr, ExprRange, FnArg, GenericArgument,
+    GenericParam, Path, Token, Type, WhereClause,
+};
 
-use crate::utils::{collect_hitori_attrs, find_unique_hitori_attr, take_hitori_attrs};
+use crate::utils::{
+    collect_hitori_attrs, expand_lifetime_generic_params_into_unit_refs, find_unique_hitori_attr,
+    remove_generic_params_bounds, take_hitori_attrs,
+};
 
 enum Repeat {
     Star,
@@ -40,6 +45,16 @@ impl Parse for Repeat {
     }
 }
 
+impl Repeat {
+    fn has_upper_bound(&self) -> bool {
+        match self {
+            Repeat::Exact(_) => true,
+            Repeat::Range(range) => range.to.is_some(),
+            _ => false,
+        }
+    }
+}
+
 type Group<'a> = &'a mut Punctuated<Expr, Token![,]>;
 
 enum TreeInner<'a> {
@@ -50,7 +65,6 @@ enum TreeInner<'a> {
 
 struct Tree<'a> {
     inner: TreeInner<'a>,
-    #[allow(dead_code)]
     repeat: Option<Repeat>,
     capture: Vec<Ident>, // used in place for calls
 }
@@ -74,135 +88,343 @@ impl<'a> TryFrom<&'a mut Expr> for Tree<'a> {
     }
 }
 
-#[derive(Default)]
+pub struct Input<'a> {
+    pub generic_params: &'a mut Punctuated<GenericParam, Token![,]>,
+    pub where_clause: Option<&'a WhereClause>,
+    pub trait_args: &'a [GenericArgument; 3],
+    pub self_path: &'a Path,
+    pub expr: &'a mut Expr,
+    pub is_mut: bool,
+}
+
 pub struct Output {
     pub tokens: TokenStream,
     pub capture: Vec<Ident>, // used for #[hitori::add_define]
 }
 
-impl AddAssign for Output {
-    fn add_assign(&mut self, rhs: Self) {
-        self.tokens.extend(rhs.tokens);
-        self.capture.extend(rhs.capture);
-    }
-}
-
-impl TryFrom<&mut Expr> for Output {
+impl<'a> TryFrom<Input<'a>> for Output {
     type Error = syn::Error;
 
-    fn try_from(expr: &mut Expr) -> syn::Result<Self> {
-        Tree::try_from(expr).and_then(TryInto::try_into)
-    }
-}
-
-impl<'a> TryFrom<Tree<'a>> for Output {
-    type Error = syn::Error;
-
-    fn try_from(tree: Tree<'a>) -> syn::Result<Self> {
-        expand_tree(tree, NeedNext(true)).map(|(mut expanded, _)| {
-            expanded.capture.sort_unstable();
-            expanded.capture.dedup();
-            if expanded.tokens.is_empty() {
-                expanded.tokens = quote! { Ok(Some(..start)) };
-            } else {
-                let body = expanded.tokens;
-                expanded.tokens = quote! {
-                    let mut iter = iter.into_iter(); let mut next: I::Item; let mut end;
-                    #body
-                    Ok(Some(..start))
-                };
+    fn try_from(mut input: Input<'a>) -> syn::Result<Self> {
+        let tree = input.expr.try_into()?;
+        let mut st = State::new(&input.trait_args[1], &input.trait_args[2]);
+        let wrapper_impl_body = st.expand_tree(tree)?;
+        Ok(if wrapper_impl_body.is_empty() {
+            Self {
+                tokens: quote! { Ok(Some(..start)) },
+                capture: vec![],
             }
-            expanded
+        } else {
+            let mut output_tokens = expand_wrapper_header(&mut input);
+            let last_subexpr_fn_ident = st.last_subexpr_fn_ident;
+            output_tokens.extend(quote! {
+                { #wrapper_impl_body }
+                let mut wrapper = __Self {
+                    __target: self,
+                    __capture: capture,
+                    __end: start,
+                    __iter: iter.into_iter(),
+                    __phantom: core::marker::PhantomData,
+                };
+                wrapper.#last_subexpr_fn_ident().map(|opt| {
+                    opt.map(|_| wrapper.__end)
+                })
+            });
+            st.capture_fn_idents.sort_unstable();
+            st.capture_fn_idents.dedup();
+            Self {
+                tokens: output_tokens,
+                capture: st.capture_fn_idents,
+            }
         })
     }
 }
 
-struct NeedNext(bool);
+fn expand_wrapper_header(
+    Input {
+        self_path,
+        generic_params,
+        where_clause,
+        is_mut,
+        trait_args: [capture_arg, idx_arg, ch_arg],
+        ..
+    }: &mut Input,
+) -> TokenStream {
+    let all_generics_params_with_bounds = quote! { #generic_params };
 
-fn expand_tree_inner_all(
-    group: Group<'_>,
-    mut need_next: NeedNext,
-) -> syn::Result<(Output, NeedNext)> {
-    let mut output = Output::default();
-    for expr in group {
-        let expanded = expand_tree(expr.try_into()?, need_next)?;
-        output += expanded.0;
-        need_next = expanded.1;
-    }
-    Ok((output, need_next))
-}
+    let mut phantom_data_params = expand_lifetime_generic_params_into_unit_refs(
+        generic_params
+            .iter()
+            .take_while(|param| matches!(param, GenericParam::Lifetime(_)))
+            .map(|param| match param {
+                GenericParam::Lifetime(l) => l,
+                _ => unreachable!(),
+            }),
+    );
 
-fn expand_tree(
-    Tree {
-        inner, mut capture, ..
-    }: Tree<'_>,
-    need_next: NeedNext,
-) -> syn::Result<(Output, NeedNext)> {
-    let (mut output, need_next) = expand_tree_inner(inner, need_next)?;
-    let has_no_tests = output.tokens.is_empty();
+    remove_generic_params_bounds(generic_params);
 
-    if !capture.is_empty() {
-        if has_no_tests {
-            output.tokens.extend(quote! { end = start.clone(); });
+    for param in generic_params.iter() {
+        if !matches!(param, GenericParam::Const(_)) {
+            param.to_tokens(&mut phantom_data_params);
         }
-        for f in &capture[..capture.len() - 1] {
-            output
-                .tokens
-                .extend(quote! { capture.#f(start.clone()..end.clone())?; });
-        }
-        let f = capture.last().unwrap();
-        output
-            .tokens
-            .extend(quote! { capture.#f(start..end.clone())?; });
     }
 
-    if !(has_no_tests && capture.is_empty()) {
-        output.tokens.extend(quote! { start = end; });
-    }
-
-    capture.append(&mut output.capture);
-    output.capture = capture;
-
-    Ok((output, need_next))
-}
-
-fn expand_tree_inner(inner: TreeInner, need_next: NeedNext) -> syn::Result<(Output, NeedNext)> {
-    Ok(match inner {
-        TreeInner::All(group) => {
-            let mut expanded = expand_tree_inner_all(group, need_next)?;
-            let output_tokens = &expanded.0.tokens;
-            if output_tokens.is_empty() {
-            } else {
-                // TODO: don't clone start if there are is no capture in the group
-                expanded.0.tokens =
-                    quote! {end = { let mut start = start.clone(); #output_tokens start };};
-            }
-            expanded
-        }
-        TreeInner::Any(group) => {
-            return Err(syn::Error::new_spanned(
-                group,
-                "any-patterns are not implemented yet",
-            ))
-        }
-        TreeInner::Test(expr) => {
-            let mut tokens = if need_next.0 {
-                quote! {
-                    if let Some(x) = iter.next() { next = x; } else { return Ok(None); }
+    let where_clause = {
+        let mut output = where_clause.as_ref().map_or_else(
+            || quote! { where },
+            |existing| {
+                if existing.predicates.empty_or_trailing() {
+                    quote! { #where_clause }
+                } else {
+                    quote! { #where_clause, }
                 }
-            } else {
-                TokenStream::default()
-            };
-            tokens.extend(quote! {
-                end = if (#expr)(next.1) { next.0 } else { return Ok(None); };
-            });
-            (
-                Output {
-                    tokens,
-                    capture: vec![],
-                },
-                NeedNext(true),
-            )
+            },
+        );
+        output.extend(quote! {
+            __I: core::iter::Iterator<Item = (#idx_arg, #ch_arg)> + Clone,
+        });
+        output
+    };
+
+    let mut_ = is_mut.then_some(<Token![mut]>::default());
+
+    let mut output = quote! {
+       struct __Self<#all_generics_params_with_bounds, __I> #where_clause {
+           __target: &#(#mut_)? #self_path,
+           __capture: &mut #capture_arg,
+           __end: #idx_arg,
+           __iter: __I,
+           __phantom: core::marker::PhantomData<(#phantom_data_params)>,
+       };
+
+       impl<#all_generics_params_with_bounds> core::ops::Deref for __Self<#generic_params>
+       #where_clause
+       {
+           type Target = #self_path;
+
+           fn deref(&self) -> &Self::Target {
+               self.__target
+           }
+       }
+    };
+
+    if *is_mut {
+        output.extend(quote! {
+            impl<#all_generics_params_with_bounds> core::ops::DerefMut for __Self<#generic_params>
+            #where_clause
+            {
+                fn deref_mut(&mut self) -> &Self::Target {
+                    self.__target
+                }
+            }
+        })
+    }
+
+    output.extend(quote! {
+        impl<#all_generics_params_with_bounds> __Self<#generic_params> #where_clause
+    });
+
+    output
+}
+
+struct State {
+    first_fn_arg: FnArg,
+    returned_last_ty: Type,
+    returned_no_last_ty: Type,
+    subexpr_index: usize,
+    returns_last: bool,
+    last_subexpr_fn_ident: Option<Ident>,
+    capture_fn_idents: Vec<Ident>,
+}
+
+#[derive(Default)]
+struct ExpandTreeInnerOutput {
+    extra: TokenStream,
+    body: TokenStream,
+}
+
+impl State {
+    fn new(idx_arg: &GenericArgument, ch_arg: &GenericArgument) -> Self {
+        Self {
+            first_fn_arg: parse_quote! { first: (#idx_arg, #ch_arg) },
+            returned_last_ty: parse_quote! {
+                core::result::Result<core::option::Option<#ch_arg>>
+            },
+            returned_no_last_ty: parse_quote! {
+                core::result::Result<core::option::Option<()>>
+            },
+            subexpr_index: 0,
+            returns_last: false,
+            last_subexpr_fn_ident: None,
+            capture_fn_idents: Vec::with_capacity(64),
         }
-    })
+    }
+
+    fn set_next_test_ident(&mut self) {
+        self.last_subexpr_fn_ident = Some(format_ident!("__subexpr{}", self.subexpr_index));
+        self.subexpr_index += 1;
+    }
+
+    fn expand_tree(
+        &mut self,
+        Tree {
+            inner,
+            repeat,
+            mut capture,
+        }: Tree,
+    ) -> syn::Result<TokenStream> {
+        let has_first = self.returns_last;
+        let is_test = matches!(inner, TreeInner::Test(_));
+
+        let ExpandTreeInnerOutput {
+            extra: mut output,
+            body: inner_body,
+        } = self.expand_tree_inner(inner)?;
+
+        let inner_returns_last = !is_test && self.returns_last;
+        let inner_return_value = if inner_returns_last {
+            quote! { first }
+        } else {
+            self.returns_last = false;
+            quote! { () }
+        };
+
+        self.set_next_test_ident();
+        output.extend(self.expand_subexpr_sig(has_first));
+        output.extend(quote! {{
+            #inner_body
+            core::result::Result::Ok(core::option::Option::Some(#inner_return_value))
+        }});
+
+        self.returns_last = repeat
+            .as_ref()
+            .map(|repeat| !repeat.has_upper_bound())
+            .unwrap_or(inner_returns_last);
+
+        if capture.is_empty() && repeat.is_none() {
+            return Ok(output);
+        }
+
+        let mut body = if capture.is_empty() {
+            TokenStream::new()
+        } else {
+            quote! { let start = self.__end.clone(); }
+        };
+
+        if repeat.is_none() {
+            let inner_call = self.expand_subexpr_call(has_first);
+            body.extend(quote! {
+                let output = #inner_call?;
+                if output.is_none() {
+                    return core::result::Result::Ok(output);
+                }
+            })
+        } else {
+            unimplemented!();
+        }
+
+        if !capture.is_empty() {
+            for f in &capture[..capture.len() - 1] {
+                body.extend(quote! { capture.#f(start.clone()..self.__end.clone())?; });
+            }
+            let f = capture.last().unwrap();
+            body.extend(quote! { capture.#f(start..self.__end.clone())?; });
+        }
+
+        self.set_next_test_ident();
+        output.extend(self.expand_subexpr_sig(has_first));
+
+        if repeat.is_none() {
+            body.extend(quote! {
+                core::result::Result::Ok(output)
+            });
+        } else {
+            unimplemented!();
+        }
+
+        output.extend(quote! { { #body } });
+
+        self.capture_fn_idents.append(&mut capture);
+
+        Ok(output)
+    }
+
+    fn expand_tree_inner(&mut self, inner: TreeInner) -> syn::Result<ExpandTreeInnerOutput> {
+        match inner {
+            TreeInner::All(group) => self.expand_tree_inner_all(group),
+            TreeInner::Any(group) => self.expand_tree_inner_any(group),
+            TreeInner::Test(expr) => Ok(self.expand_tree_inner_test(expr)),
+        }
+    }
+
+    fn expand_tree_inner_all(&mut self, group: Group) -> syn::Result<ExpandTreeInnerOutput> {
+        let mut output = ExpandTreeInnerOutput::default();
+        for expr in group {
+            let has_first = self.returns_last;
+            output.extra.extend(self.expand_tree(expr.try_into()?)?);
+            let call = self.expand_subexpr_call(has_first);
+            output.body.extend(if self.returns_last {
+                quote! { let first = if let Some(first) = #call? { first } else { return None; } }
+            } else {
+                quote! { if #call?.is_none() { return None } }
+            });
+        }
+        Ok(output)
+    }
+
+    fn expand_tree_inner_any(&mut self, group: Group) -> syn::Result<ExpandTreeInnerOutput> {
+        Err(syn::Error::new_spanned(
+            group,
+            "any-patterns are not implemented yet",
+        ))
+    }
+
+    fn expand_tree_inner_test(&self, expr: &Expr) -> ExpandTreeInnerOutput {
+        let mut body = if self.returns_last {
+            TokenStream::default()
+        } else {
+            quote! {
+                let first = if let Some(first) = self.__iter.next() {
+                    first
+                } else {
+                    return core::result::Result::Ok(core::option::Option::None);
+                };
+            }
+        };
+        body.extend(quote! {
+            if (#expr)(first.1) {
+                self.__end = first.0
+            } else {
+                return core::result::Result::Ok(core::option::Option::None);
+            };
+        });
+        ExpandTreeInnerOutput {
+            body,
+            extra: TokenStream::new(),
+        }
+    }
+
+    fn expand_subexpr_sig(&self, has_first: bool) -> TokenStream {
+        let test_ident = &self.last_subexpr_fn_ident;
+        let first = has_first.then_some(&self.first_fn_arg);
+        let mut output = quote! {
+            fn #test_ident(&mut self, #(#first)?) ->
+        };
+        if self.returns_last {
+            &self.returned_last_ty
+        } else {
+            &self.returned_no_last_ty
+        }
+        .to_tokens(&mut output);
+        output
+    }
+
+    fn expand_subexpr_call(&self, has_first: bool) -> TokenStream {
+        let test_ident = &self.last_subexpr_fn_ident;
+        let first = has_first.then_some(&self.first_fn_arg);
+        quote! {
+            #test_ident(self, #(#first)?)
+        }
+    }
 }
